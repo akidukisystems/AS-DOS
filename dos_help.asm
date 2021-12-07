d2c255cced92986f8954f6e4495f7ec24f3f53f6
akidukisystems <admin@akidukisystems.com>
2021-12-07 20:16:24 +0900

    新しいヘルプの追加

--- dos_help.asm
+++ dos_help.asm
@@ -8,55 +8,62 @@
 DCMD_HELP_detail_NotMatch:
 	MOV		SI, MSG_CRLF
 	CALL	print
-	JMP 	Key_Ret
+	JMP 	ReturnAdrs
 
 DCMD_HELP_MSG1:
 	DB		"Active commands :", 0x0D, 0x0A
-	DB		" reset     romb     help     lfchk    exit     cls      mem     ", 0x0D, 0x0A, 0x00
+    DB      " reset    romb     help     lfchk    exit    ", 0x0D, 0x0A
+    DB      " cls      mem      info", 0x0D, 0x0A, 0x00
 
 DCMD_HELP_detail:
 
     MOV		BX, _CMD_PRM0
 	MOV		SI, DCMD_RESET
 	CALL	Compare
-    OR      AX, AX
+    OR      AL, AL
     JZ      DCMD_HELP_detail_RESET
 
     MOV     BX, _CMD_PRM0
     MOV     SI, DCMD_HELP
     CALL    Compare
-    OR      AX, AX
+    OR      AL, AL
     JZ      DCMD_HELP_detail_HELP
 
     MOV     BX, _CMD_PRM0
     MOV     SI, DCMD_ROMB
     CALL    Compare
-    OR      AX, AX
+    OR      AL, AL
     JZ      DCMD_HELP_detail_ROMB
 
     MOV     BX, _CMD_PRM0
     MOV     SI, DCMD_LFCHK
     CALL    Compare
-    OR      AX, AX
+    OR      AL, AL
     JZ      DCMD_HELP_detail_LFCHK
 
     MOV     BX, _CMD_PRM0
     MOV     SI, DCMD_EXIT
     CALL    Compare
-    OR      AX, AX
+    OR      AL, AL
     JZ      DCMD_HELP_detail_EXIT
 
     MOV     BX, _CMD_PRM0
     MOV     SI, DCMD_CLS
     CALL    Compare
-    OR      AX, AX
+    OR      AL, AL
     JZ      DCMD_HELP_detail_CLS
 
     MOV     BX, _CMD_PRM0
     MOV     SI, DCMD_MEM
     CALL    Compare
-    OR      AX, AX
+    OR      AL, AL
     JZ      DCMD_HELP_detail_MEM
+
+    MOV     BX, _CMD_PRM0
+    MOV     SI, DCMD_INFO
+    CALL    Compare
+    OR      AL, AL
+    JZ      DCMD_HELP_detail_INFO
 
     JMP     DCMD_HELP_detail_NotMatch
 
@@ -96,7 +103,7 @@ DCMD_HELP_detail_LFCHK:
     JMP     DCMD_HELP_detail_NotMatch
 
 DCMD_HELP_detail_LFCHK_MSG:
-    DB      "Type loaded floppy disk sector.", 0x0D, 0x0A, " > lfchk", 0x0D, 0x0A, 0x00
+    DB      "Print loaded floppy disk sector.", 0x0D, 0x0A, " > lfchk", 0x0D, 0x0A, 0x00
 
 DCMD_HELP_detail_EXIT:
     MOV     SI, DCMD_HELP_detail_EXIT_MSG
@@ -124,3 +131,15 @@ DCMD_HELP_detail_MEM:
 
 DCMD_HELP_detail_MEM_MSG:
     DB      "Print installed memory size. (64KB)", 0x0D, 0x0A, " > mem", 0x0D, 0x0A, 0x00
+
+DCMD_HELP_detail_INFO:
+    MOV     SI, DCMD_HELP_detail_INFO_MSG
+    CALL    print
+    
+    JMP     DCMD_HELP_detail_NotMatch
+
+DCMD_HELP_detail_INFO_MSG:
+    DB      "Print infomation data.", 0x0D, 0x0A, " > info <type>", 0x0D, 0x0A
+    DB      " <type> parameter...", 0x0D, 0x0A
+    DB      "        lfchk = Print loaded floppy disk sector.", 0x0D, 0x0A
+    DB      "        mem   = Print installed memory size.", 0x0D, 0x0A, 0x00
